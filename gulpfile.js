cd511047d6c7dfd0c82e582cbaa397fdd3c71bb2
Lucas Calje <lucas@frontmen.nl>
2015-10-10 22:32:36 +0200

    Applied some awesome styling :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,36 @@
 var gulp = require('gulp');
+//var sass = require('gulp-sass');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var $ = gulpLoadPlugins();
 
+console.dir($);
 var b = require('browserify')();
 
 // Transpile all JS to ES5.
-gulp.task('js', function () {
- return gulp.src(['app/**/*.{js,html}'])
-   .pipe($.sourcemaps.init())
-   .pipe($.if('*.html', $.crisper())) // Extract JS from .html files
-   .pipe($.if('*.js', $.babel()))
-   .pipe($.sourcemaps.write('.'))
-   .pipe(gulp.dest('.tmp/'))
-   .pipe(gulp.dest('dist/'));
+gulp.task('js', function() {
+  return gulp.src(['app/**/*.{js,html}'])
+    .pipe($.sourcemaps.init())
+    .pipe($.if('*.html', $.crisper())) // Extract JS from .html files
+    .pipe($.if('*.js', $.babel()))
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('.tmp/'))
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('default', ['js']);
+gulp.task('default', ['copy', 'js', 'sass']);
 
-gulp.task('watch', ['js'], function () {
+gulp.task('watch', ['copy', 'js', 'sass'], function() {
   gulp.watch(['app/**/*.{js,html}'], ['js']);
+  gulp.watch(['app/scss/*.scss'], ['sass']);
 });
 
+gulp.task('sass', function() {
+  gulp.src('./app/scss/**/*.scss')
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('copy', function() {
+  gulp.src('assets/img/*.*')
+    .pipe(gulp.dest('./dist/img'));
+});
