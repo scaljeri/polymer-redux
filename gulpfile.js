366e5eae2e0894bc731a4f07d6927e72754efaa8
Lucas Calje <lucas@frontmen.nl>
2015-10-22 22:03:53 +0200

    Fixed build process (inject css issue)

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,63 @@
 var $,
-    fs   = require('fs'),
-    del  = require('del'),
+    fs = require('fs'),
+    del = require('del'),
     gulp = require('gulp'),
     path = require('path'),
-    vinylPaths = require('vinyl-paths'),
+    runSequence = require('run-sequence'),
     gulpLoadPlugins = require('gulp-load-plugins');
 
-$    = gulpLoadPlugins();
+$ = gulpLoadPlugins();
 
-gulp.task('default', ['copy', 'js', 'inject-css']);
+gulp.task('default', ['clean'], function () {
+    runSequence('copy', 'js', 'inject-css');
+});
+
+gulp.task('clean', function () {
+    del.sync('./dist/');
+});
 
 // Transpile all JS to ES5.
-gulp.task('js', function () {
-  gulp.src(['app/**/*.{js,html}'])
-      .pipe($.sourcemaps.init())
-      .pipe($.if('*.html', $.crisper())) // Extract JS from .html files
-      .pipe($.if('*.js', $.babel()))
-      .pipe($.sourcemaps.write('.'))
-      .pipe(gulp.dest('.tmp/'))
-      .pipe(gulp.dest('dist/'));
+gulp.task('js', function() {
+    return gulp.src(['app/**/*.{js,html}'])
+        .pipe($.sourcemaps.init())
+        .pipe($.if('*.html', $.crisper())) // Extract JS from .html files
+        .pipe($.if('*.js', $.babel()))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('.tmp/'))
+        .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('watch', ['copy', 'js', 'inject-css'], function () {
-  gulp.watch(['app/**/*.{js,html}'], ['js']);
-  gulp.watch(['app/scss/*.scss'], ['inject-css']);
+gulp.task('watch', ['copy', 'js', 'inject-css'], function() {
+    return gulp.watch(['app/**/*.{js,html}', 'app/scss/*.scss'], function () {
+        runSequence('clean', 'copy', 'js', 'inject-css');
+    });
 });
 
-gulp.task('sass', function () {
-  gulp.src('./app/scss/**/*.scss')
-      .pipe($.sass().on('error', $.sass.logError))
-      .pipe(gulp.dest('./dist/css'));
+gulp.task('sass', function() {
+    return gulp.src('./app/scss/**/*.scss')
+        .pipe($.sass().on('error', $.sass.logError))
+        .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('copy', function () {
-  gulp.src('assets/img/*.*')
-      .pipe(gulp.dest('./dist/img'));
+gulp.task('copy', function() {
+    return gulp.src('assets/img/*.*')
+        .pipe(gulp.dest('./dist/img'));
 });
 
 var clean = require('gulp-clean');
 
-gulp.task('inject-css', ['js', 'sass'], function () {
-  gulp.src('dist/html/**/*.html')
-      .pipe($.tap(function (file, t) {
-        var filename = path.basename(file.path).replace(/html$/, 'css');
+gulp.task('inject-css', ['sass'], function() {
+    return gulp.src('./dist/html/**/*.html', {base: './dist/html'})
+        .pipe($.tap(function(file, t) {
+            var filename = path.basename(file.path).replace(/html$/, 'css');
 
-        return gulp.src(file.path)
-            .pipe($.replace(/\/\* inject:css \*\//, function () {
-                  var style = fs.readFileSync('./dist/css/' + filename, 'utf8');
-                  return style;
-                }))
-            .pipe(clean())
-            //.pipe(vinylPaths(del.sync))
-            .pipe(gulp.dest('./dist/html'));
-      }));
+            return gulp.src(file.path, {base: './dist/html'})
+                .pipe($.replace(/\/\* inject:css \*\//,
+                    function() {
+                        var style = fs.readFileSync('./dist/css/' + filename, 'utf8');
+                        return style;
+                    }))
+                .pipe(clean())
+                .pipe(gulp.dest('./dist/html'));
+        }));
 });
